3a2a0feaab64cf54a2c4cab090d5d7bd9e4af67c
eggwise <dev@eggwise.com>
2017-05-09 11:51:03 +0200

    damn gitignore

--- test/test_1.py
+++ test/test_1.py
@@ -3,11 +3,8 @@ from facebook_messenger_api import Message
 
 from .utils import get_test_bot, get_test_recipient
 
-
 bot = get_test_bot()
 recipient = get_test_recipient()
-
-
 
 response, success = bot.send_message('This is a test', recipient)
 
